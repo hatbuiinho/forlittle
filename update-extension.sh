9daecf0730856b8a4b09db19601ad02b08a70f49
Huy Tuyen <fsoft.datnn@gmail.com>
2026-05-13 06:06:39 +0700

    Add update script for packaging Chrome extensions to streamline release process.

---
+++ update-extension.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+UPDATE_BASE_URL='https://little-be.hatbuinho.me/extensions/forlittle' \
+PEM_PATH='./extension.pem' \
+CRX_PATH='./extension.crx' \
+./scripts/package-extension.sh
